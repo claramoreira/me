ab41b7b282cea75e1cebba3f8fe9b9fe9e43895a
claramoreira <51670485+claramoreira@users.noreply.github.com>
2020-05-08 10:50:01 -0300

    bug fixing

---
+++ build/precache-manifest.af2d9df4e4599267ef03b05a3dc1b269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d78d8e764295f38283b6a91d4d923f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cdf61967dd03e69a277",
+    "url": "/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "cb75972a0a6b6696ca35",
+    "url": "/static/css/main.4ee798f0.chunk.css"
+  },
+  {
+    "revision": "3cdf61967dd03e69a277",
+    "url": "/static/js/2.ff657df1.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.ff657df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb75972a0a6b6696ca35",
+    "url": "/static/js/main.8ce260ab.chunk.js"
+  },
+  {
+    "revision": "018148c430b305b97fcb",
+    "url": "/static/js/runtime-main.419b93a7.js"
+  }
+]);
